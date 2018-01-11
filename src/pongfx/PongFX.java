d9084eac9518910c03a0eedc2bcdb41a68add87f
1DAW08 <1DAW08@DESKTOP-PQRAPVS>
2018-01-11 10:43:51 +0100

    Movimiento bola horizontal

--- src/pongfx/PongFX.java
+++ src/pongfx/PongFX.java
@@ -5,28 +5,63 @@
  */
 package pongfx;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 /**
  *
  * @author 1DAW08
  */
 public class PongFX extends Application {
+    // PANEL
+    public static final double anchoPanel = 590;
+    public static final double altoPanel = 300;
+    //VELOCIDAD
+    public static double velocidadPelota = 1;
     
+    //Rectangulo
+    private int xRectangulo = 0;
+    private int yRectangulo = 0;
+    private int anchoRectangulo = 15;
+    private int altoRectangulo = 60; 
     @Override
     public void start(Stage primaryStage) {
-        StackPane root = new StackPane();
-        Scene scene = new Scene (root, 300, 250);
+        Pane root = new Pane();
+        // Crear pantalla
+        Scene scene = new Scene (root, 600, 400, Color.BLACK);
         primaryStage.setTitle("PongFx");
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
+        Circle pelota = new Circle(10, 30, 7, Color.WHITE); // Primer valor CentroY segundo centroX y tercero radio
+        root.getChildren().add(pelota);
+        pelota.setTranslateX(300);
+        pelota.setTranslateY(200);
+        //Crear barra
+        Rectangle barra = new Rectangle(anchoRectangulo, altoRectangulo, Color.WHITE);
+        root.getChildren().add(barra);
+        barra.setTranslateX(1);
+        barra.setTranslateY(200);
+       //Mover rectanguloç
+
+        //Movimiento pelota
+        AnimationTimer movimientoPelota = new AnimationTimer() {
+             public void handle(long now) {
+                 //Direccion de bola
+                 if(pelota.getTranslateX() < 0 || pelota.getTranslateX() > anchoPanel) {
+                    velocidadPelota *= -1;
+                }  
+                 pelota.setTranslateX(pelota.getTranslateX() + velocidadPelota);
+            }
+        };
+             movimientoPelota.start();
+        }
         
 
     /**
